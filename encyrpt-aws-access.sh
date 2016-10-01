bc2c7c393bacc351c533903ce322a0147ea29dea
Andrew Macheret <andrew.macheret@sap.com>
2016-10-01 15:53:37 -0700

    Fix aws access keys

--- encyrpt-aws-access.sh
+++ encyrpt-aws-access.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo '  access_key_id:'
-awk '$1 == "aws_access_key_id" {print $3}' ~/.aws/credentials | travis encrypt access_key_id | sed 's/^/    secure: /'
+travis encrypt "$( awk '$1 == "aws_access_key_id" {print $3}' ~/.aws/credentials )" | sed 's/^/    secure: /'
 
 echo '  secret_access_key:'
-awk '$1 == "aws_secret_access_key" {print $3}' ~/.aws/credentials | travis encrypt secret_access_key | sed 's/^/    secure: /'
+travis encrypt "$( awk '$1 == "aws_secret_access_key" {print $3}' ~/.aws/credentials )" | sed 's/^/    secure: /'
