5e88a92778e15d6793d8c80e3d69f48cb619a49e
Andrew Macheret <andrew.macheret@gmail.com>
2016-10-06 19:25:43 -0700

    Remove scripts

--- encrypt-aws-access.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-echo '  access_key_id:'
-(travis encrypt "$( awk '$1 == "aws_access_key_id" {print $3}' ~/.aws/credentials )" | sed 's/^/    secure: /') 2>&1 | grep -v '^detected repository '
-
-echo '  secret_access_key:'
-(travis encrypt "$( awk '$1 == "aws_secret_access_key" {print $3}' ~/.aws/credentials )" | sed 's/^/    secure: /') 2>&1 | grep -v '^detected repository '
